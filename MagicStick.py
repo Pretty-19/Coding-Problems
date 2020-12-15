b3883f5f5166d838904ed693660786df82eeb1ca
Loveena Saran <42793632+Pretty-19@users.noreply.github.com>
2020-12-15 15:20:29 +0530

    Create MagicStick.py

---
+++ MagicStick.py
@@ -0,0 +1,71 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+
+def fittingAnalysis(height_of_locker ,length_of_locker,width_of_locker,length_of_magic_stick):
+
+        if height_of_locker length_of_magic_stick:
+
+        digonal = pow(height_of_locker, 2) + pow(length_of_locker, 2)+ pow(width_of_locker,2)
+        cuboid_digonal=math.sqrt(digonal)
+        return cuboid_digonal
+
+
+if __name__ == '__main__':
+    height_of_locker,length_of_locker,width_of_locker,length_of_magic_stick= map(int,input().split())
+
+    result = fittingAnalysis(height_of_locker ,length_of_locker,width_of_locker,length_of_magic_stick)
+    print(result)
+
+
+
+    
+    
+ 
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+def cementDistribution(increaseRate, sites):
+    if len(sites) == 1 and len(increaseRate) == 1:
+        return [int((((increaseRate[0] ** sites[0])-increaseRate[0])/(increaseRate[0]-1))+1), (increaseRate[0] ** sites[0])]
+
+#logic
+
+if __name__ == '__main__':
+
+    increaseRate_count = int(input().strip())
+
+    increaseRate = []
+
+    for _ in range(increaseRate_count):
+        increaseRate_item = int(input().strip())
+        increaseRate.append(increaseRate_item)
+
+    sites_count = int(input().strip())
+
+    sites = []
+
+    for _ in range(sites_count):
+        sites_item = int(input().strip())
+        sites.append(sites_item)
+
+    result = cementDistribution(increaseRate, sites)
+
+str_join = ",".join(map(str, result))
+print(str_join)
+   
+    
+    
