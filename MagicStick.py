48c3a595566f7aa7b281bcffeb47b97611cd7559
Loveena Saran <42793632+Pretty-19@users.noreply.github.com>
2020-12-15 15:22:32 +0530

    Update MagicStick.py

--- MagicStick.py
+++ MagicStick.py
@@ -30,42 +30,5 @@ if __name__ == '__main__':
     
  
 
-import math
-import os
-import random
-import re
-import sys
-
-
-
-def cementDistribution(increaseRate, sites):
-    if len(sites) == 1 and len(increaseRate) == 1:
-        return [int((((increaseRate[0] ** sites[0])-increaseRate[0])/(increaseRate[0]-1))+1), (increaseRate[0] ** sites[0])]
-
-#logic
-
-if __name__ == '__main__':
-
-    increaseRate_count = int(input().strip())
-
-    increaseRate = []
-
-    for _ in range(increaseRate_count):
-        increaseRate_item = int(input().strip())
-        increaseRate.append(increaseRate_item)
-
-    sites_count = int(input().strip())
-
-    sites = []
-
-    for _ in range(sites_count):
-        sites_item = int(input().strip())
-        sites.append(sites_item)
-
-    result = cementDistribution(increaseRate, sites)
-
-str_join = ",".join(map(str, result))
-print(str_join)
-   
     
     
